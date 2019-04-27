09ce99ef1176e3d85c8a6538ac602effafe68472
insuk <cis1725@gmail.com>
2019-04-27 21:40:02 +0900

    Spring boot test & travis CI

---
+++ src/main/java/com/community/book/BookSeviceImpl.java
@@ -0,0 +1,13 @@
+package com.community.book;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookSeviceImpl implements BookService{
+    @Override
+    public List<Book> getBookList() {
+        return null;
+    }
+}
